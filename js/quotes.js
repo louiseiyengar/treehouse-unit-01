addf244e4deb5418889ac8268d82b746731a0cf2
Louise Iyengar <lki@louiseiyengar.com>
2019-01-24 19:31:23 -0500

    Add quotes.js file with completed quotes array

---
+++ js/quotes.js
@@ -0,0 +1,122 @@
+quotes = 
+[
+    {
+        quote: "I never travel without my diary. One should always have something sensational to read in the train.",
+        source: "The Importance of Being Ernest",
+        author: "Oscar Wilde",
+        character: "Hon. Gwendolen Fairfax",
+        date: "1898"
+    },
+    {
+        quote: "When I was young I thought that money was the most important thing in life; now that I am old I know that it is.",
+        source: "",
+        author: "Oscar Wilde",
+        character: "",
+        date: ""
+    },
+    {
+        quote: "Be yourself; everyone else is already taken.",
+        source: "",
+        author: "Oscar Wilde",
+        character: "",
+        date: ""
+    },
+    {
+        quote: "I can resist anything except temptation.",
+        source: "Lady Windermere's Fan",
+        author: "Oscar Wilde",
+        character: "Lord Darlington",
+        date: "1892"
+    },
+    {
+        quote: "Nowadays people know the price of everything and the value of nothing.",
+        source: "The Picture of Dorian Gray",
+        author: "Oscar Wilde",
+        character: "Lord Henry Wotton",
+        date: "1890"
+    },
+    {
+        quote: "It is absurd to divide people into good and bad. People are either charming or tedious.",
+        source: "Lady Windermere's Fan",
+        author: "Oscar Wilde",
+        character: "Lord Darlington",
+        date: "1892"
+    },
+    {
+        quote: "No good deed goes unpunished.",
+        source: "",
+        author: "Oscar Wilde",
+        character: "",
+        date: ""
+    },
+    {
+        quote: "If the law supposes that, the law is a ass — a idiot.",
+        source: "Oliver Twist",
+        author: "Charles Dickens",
+        character: "Mr. Bumble",
+        date: "1837"
+    },
+    {
+        quote: "There are books of which the backs and covers are by far the best parts.",
+        source: "Oliver Twist",
+        author: "Charles Dickens",
+        character: "Mr. Brownlow",
+        date: "1837"
+    },
+    {
+        quote: "What greater gift than the love of a cat.",
+        source: "",
+        author: "Charles Dickens",
+        character: "",
+        date: ""
+    },
+    {
+        quote: "Procrastination is the thief of time, collar him.",
+        source: "David Copperfield",
+        author: "Charles Dickens",
+        character: "Mr. Micawber",
+        date: "1849"
+    },
+    {
+        quote: "Ask no questions, and you'll be told no lies.",
+        source: "Great Expectations",
+        author: "Charles Dickens",
+        character: "Mrs Joe Gargery",
+        date: "1860"
+    },
+    {
+        quote: "I have not the pleasure of understanding you.",
+        source: "Pride and Prejudice",
+        author: "Jane Austen",
+        character: "Mr. Bennet",
+        date: "1797"
+    },
+    {
+        quote: "Laugh as much as you choose, but you will not laugh me out of my opinion.",
+        source: "Pride and Prejudice",
+        author: "Jane Austen",
+        character: "Jane Bennet",
+        date: "1797"
+    },
+    {
+        quote: "Friendship is certainly the finest balm for the pangs of disappointed love.",
+        source: "",
+        author: "Jane Austen",
+        character: "",
+        date: ""
+    },
+    {
+        quote: "Nothing ever fatigues me, but doing what I do not like.",
+        source: "",
+        author: "Jane Austen",
+        character: "",
+        date: ""
+    },
+    {
+        quote: "I always deserve the best treatment because I never put up with any other.",
+        source: "Emma",
+        author: "Jane Austen",
+        character: "Emma Woodhouse",
+        date: "1815"
+    }
+];
